d03c92e4af5e23a00a1547ef51d0c607a0643876
Tim Galeckas <tim@galeckas.com>
2011-12-05 11:01:19 -0600

    now self balancing

---
+++ app/models/self_balancing_binary.rb
@@ -0,0 +1,127 @@
+module SelfBalancingBinary
+
+  class Tree
+    attr_accessor :root
+    def root
+      @root ||= Node.new
+    end
+
+    include Enumerable
+    delegate :each, :find, :to => :root
+
+    def <<( value )
+      Array(value).each do |v|
+        self.root << v
+        self.root = SelfBalancingBinary.balance(self.root)
+      end
+      self
+    end
+
+  end
+
+  class Node
+    include Enumerable
+    attr_accessor :left_child, :right_child, :hash_key, :bucket
+
+    def <<( value_or_values )
+      new_values = Array( value_or_values )
+      while new_value = new_values.pop
+        new_hash = new_value.hash
+        case
+        when self.hash_key.nil?
+          add_to_bucket( new_value )
+        when new_hash < self.hash_key
+          self.left_child ||= Node.new
+          self.left_child << new_value
+        when new_hash > self.hash_key
+          self.right_child ||= Node.new
+          self.right_child << new_value
+        else
+          add_to_bucket( new_value )
+        end
+      end
+      self
+    end
+
+    def each( &block )
+      self.left_child && self.left_child.each( &block )
+      self.hash_key.nil? || self.bucket.each( &block )
+      self.right_child && self.right_child.each( &block )
+    end
+
+    def find( value_to_find )
+      hash_to_find = value_to_find.hash
+      case
+      when self.hash_key.nil?
+        nil
+      when hash_to_find == self.hash_key
+        self.bucket.find{|v| v == value_to_find }
+      when hash_to_find < self.hash_key
+        self.left_child && self.left_child.find( value_to_find )
+      when hash_to_find > self.hash_key
+        self.right_child && self.right_child.find( value_to_find )
+      end
+    end
+
+    def depth
+      [left_depth,right_depth].max + 1
+    end
+
+    def left_depth
+        self.left_child && self.left_child.depth || 0
+    end
+
+    def right_depth
+        self.right_child && self.right_child.depth || 0
+    end
+
+    def left_heavy?
+      ( left_depth - right_depth ) > 1
+    end
+
+    def right_heavy?
+      ( right_depth - left_depth ) > 1
+    end
+
+    def to_s( depth = 0 )
+      ( self.left_child  ? self.left_child.to_s( depth + 1 ) : ''  ) +
+      ((self.bucket      ? (('  ' * depth) + self.bucket.join(', ' ) + "\n") : '')) +
+      ( self.right_child ? self.right_child.to_s( depth + 1 ) : '' )
+    end
+
+  protected
+    def add_to_bucket( value )
+      self.hash_key ||= value.hash
+      self.bucket ||= []
+      self.bucket << value
+    end
+  end
+  protected
+  class << self
+    def rotate_right( node )
+      pivot = node.left_child
+      node.left_child = pivot.right_child
+      pivot.right_child = node
+      pivot
+    end
+    def rotate_left( node )
+      pivot = node.right_child
+      node.right_child = pivot.left_child
+      pivot.left_child = node
+      pivot
+    end
+    def balance( node )
+      return node if node.nil?
+      node.left_child = balance( node.left_child )
+      node.right_child = balance( node.right_child )
+      case
+      when node.left_heavy?
+        rotate_right( node )
+      when node.right_heavy?
+        rotate_left( node )
+      else
+        node
+      end
+    end
+  end
+end
---
+++ test/unit/self_balancing_binary_tree_test.rb
@@ -0,0 +1,74 @@
+require './test_helper'
+
+class SelfBalancingBinaryTreeTest < ActiveSupport::TestCase
+
+  #copied from http://rubydoc.info/gems/shoulda/2.11.3/Shoulda/Assertions#assert_same_elements-instance_method
+  def assert_same_elements(a1, a2, msg = nil)
+    [:select, :inject, :size].each do |m|
+      [a1, a2].each {|a| assert_respond_to(a, m, "Are you sure that #{a.inspect} is an array?  It doesn't respond to #{m}.") }
+    end
+
+    assert a1h = a1.inject({}) { |h,e| h[e] = a1.select { |i| i == e }.size; h }
+    assert a2h = a2.inject({}) { |h,e| h[e] = a2.select { |i| i == e }.size; h }
+
+    assert_equal(a1h, a2h, msg)
+  end
+
+  test "insert" do
+    n = SelfBalancingBinary::Tree.new
+    assert_equal [], n.to_a
+    n << 5
+    assert_equal [5], n.to_a
+    n << 5
+    assert_equal [5,5], n.to_a
+    n << 4
+    assert_same_elements [4,5,5], n.to_a
+    n << [ 4,5 ]
+    assert_same_elements [4,4,5,5,5], n.to_a
+    n = SelfBalancingBinary::Tree.new
+    n << [ 6,5,4,3,2,1 ]
+    assert_same_elements [1,2,3,4,5,6], n.to_a
+    assert_nil n.find(8)
+    assert_equal 5, n.find(5)
+    assert_equal 1, n.find(1)
+    assert_equal 6, n.find(6)
+  end
+
+  test "insert multiple types" do
+    n = SelfBalancingBinary::Tree.new
+    n << "hello"
+    n << 5
+  end
+
+  test "insert a hash collision" do
+    class HashCollidingString < String
+      def hash
+        1
+      end
+    end
+    n = SelfBalancingBinary::Tree.new
+
+    hello  = HashCollidingString.new('hello')
+    hello2 = HashCollidingString.new('hello2')
+
+    n << [ hello, hello2 ]
+    assert_equal hello, n.find(hello)
+    assert_equal hello2, n.find(hello2)
+  end
+
+  test 'self balance' do
+    n = 100
+    sample_size = 10
+    1.upto(sample_size) do
+      t = SelfBalancingBinary::Tree.new
+      t << 1.upto(n).to_a.shuffle
+      assert_operator t.root.depth, :<=, 2*(Math.log(n) / Math.log(2))
+    end
+    t = SelfBalancingBinary::Tree.new
+    t << 1.upto(n).to_a
+    assert_operator t.root.depth, :<=, 2*(Math.log(n) / Math.log(2))
+    t = SelfBalancingBinary::Tree.new
+    t << 1.upto(n).map{|i|n+1-i}
+    assert_operator t.root.depth, :<=, 2*(Math.log(n) / Math.log(2))
+  end
+end
